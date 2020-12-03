a715db17ed6fdd2cf73ea50f207d9e5d7c8f7e1f
Jared Langson <langson.jared@gmail.com>
2020-12-03 16:53:53 -0500

    First commmit day4

---
+++ day4/Main.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("yo");
+        List<String> fileStream = Files.readAllLines(Paths.get("input.txt"));
+        
+
+    }
+}
