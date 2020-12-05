ad006eaf7ef5f6a29c63ac0cb73a33c3f2fff413
Jared Langson <langson.jared@gmail.com>
2020-12-05 03:32:20 -0500

    day5 part 1 solved. Fixed an irritating off by one error

---
+++ day5/BoardingPass.java
@@ -0,0 +1,53 @@
+public class BoardingPass {
+    public final String rowStr;
+    public final String colStr;
+    public final int id;
+    public final int row;
+    public final int col;
+    
+    public BoardingPass(String string){
+        this.rowStr = string.substring(0, 7);
+        this.colStr = string.substring(7, string.length());
+       //function to calulate row
+       this.row = calcGeneral(this.rowStr, 'F');
+       
+       //func to calc col
+       this.col = calcGeneral(this.colStr, 'L');
+
+        this.id = this.row * 8 + this.col;
+    }
+
+    //takes string and character to match with
+    //FL should be F or L never B or R
+    private int calcGeneral(String str, char FL){
+       //min and max are adjusted to represent the current range the seat could be in
+       int min = 0;
+       int max =(int) Math.pow(2, str.length())-1;
+
+           System.out.printf("Min is %d. Max is %d. %n", min, max);
+       for(int i=0; i < str.length(); i++){
+            int adjust = (int) Math.pow(2,str.length()-1-i);
+            if(str.charAt(i)==FL){
+                max-=adjust;
+            //    System.out.printf("Min is %d. Max is %d. adjust is -%d. i is %d.%n", min, max, adjust, i);
+            } else{ 
+                min+=adjust;
+            //    System.out.printf("Min is %d. Max is %d. adjust is +%d. i is %d.%n", min, max, adjust, i);
+            }
+       }
+    //    System.out.println("\n\n====\n");
+       
+            //min and max should be the same
+           return min;
+       }
+
+
+    
+
+	@Override
+	public String toString() {
+		return "BoardingPass [col=" + col + ", colStr=" + colStr + ", id=" + id + ", row=" + row + ", rowStr=" + rowStr
+				+ "]";
+	}
+
+}
--- day5/Main.java
+++ day5/Main.java
@@ -1,23 +1,28 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.LinkedList;
 import java.util.List;
+
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("yo");
         List<String> fileStream = Files.readAllLines(Paths.get("input.txt"));
+        LinkedList<BoardingPass> allBoardingPasses = new LinkedList<>();
+        int maxId=Integer.MIN_VALUE;
+         for(String f :fileStream){
+             BoardingPass pass = new BoardingPass(f);
+             allBoardingPasses.add(pass);
+            //  System.out.println(pass);
+             if(pass.id > maxId){
+                 maxId = pass.id;
+             }
+         }
+        // BoardingPass testPass = new BoardingPass("FBFBBFFRLR");
+        // System.out.println(testPass); //expect row 44 and col 5
+        System.out.println(maxId);
         
-                                                                                // FBFBBBFRLL
-                                                                                // BFFFBFFRRR
-                                                                                // BBFBFFFLLR
-                                                                                // FBFFFBFLRR
-                                                                                // FBFFBFBLLR
-                                                                                // BFBBFBBRLL
-                                                                                // BFFFFFFRLR
-                                                                                // FBBBBFFLLR
-                                                                                // FFBBFFFLRL
-                                                                                // FFBFBBFLLR
-
     }
 }
